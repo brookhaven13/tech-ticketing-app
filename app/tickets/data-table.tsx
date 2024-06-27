224369fc45696eb481f1480969094f67b419098b
Harley Lin <harley.lin@coretronic.com>
2024-06-27 17:35:45 +0800

    設定 Data Table & Status Badge

--- app/tickets/data-table.tsx
+++ app/tickets/data-table.tsx
@@ -1,12 +1,56 @@
-import { Ticket } from "@prisma/client"
-
+import TicketStatusBadge from "@/components/ticket-status-badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Ticket } from "@prisma/client";
 
 interface Props {
-  tickets: Ticket[]
+  tickets: Ticket[];
 }
 
 export default function DataTable({ tickets }: Props) {
   return (
-    <div>DataTable</div>
-  )
+    <div className="w-full px-10 mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>Created At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id}>
+                    <TableCell className="font-medium">
+                      {ticket.title}
+                    </TableCell>
+                    <TableCell><TicketStatusBadge status={ticket.status} /></TableCell>
+                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "2-digit",
+                        hour12: true,
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
---
+++ components/ticket-status-badge.tsx
@@ -0,0 +1,25 @@
+import { Status } from "@prisma/client";
+import { Badge } from "./ui/badge";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "bg-red-400" | "bg-blue-400" | "bg-green-400" }
+> = {
+  OPEN: { label: "Open", color: "bg-red-400" },
+  STARTED: { label: "Started", color: "bg-blue-400" },
+  CLOSED: { label: "Closed", color: "bg-green-400" },
+};
+
+export default function TicketStatusBadge({ status }: Props) {
+  return (
+    <div>
+      <Badge className={statusMap[status].color}>
+        {statusMap[status].label}
+      </Badge>
+    </div>
+  );
+}
