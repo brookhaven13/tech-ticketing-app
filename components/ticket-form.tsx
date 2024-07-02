9721d7a9d298de8001a9a02a674bd71784365632
Harley Lin <harley.lin@coretronic.com>
2024-07-02 15:15:06 +0800

    ticket form 新增其他欄位

--- components/ticket-form.tsx
+++ components/ticket-form.tsx
@@ -26,40 +26,80 @@ export default function TicketForm() {
     <div className="rounded-md border w-full p-4">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField control={form.control} name="title" render={({ field }) => (
-            <FormItem>
-              <FormLabel>Ticket Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Ticket title" {...field} />
-              </FormControl>
-            </FormItem>
-          )}></FormField>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ticket Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Ticket title" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          ></FormField>
 
-          <Controller control={form.control} name="description" render={({ field }) => (
-            <SimpleMDE placeholder="Description" />
-          )} />
+          <Controller
+            control={form.control}
+            name="description"
+            render={({ field }) => <SimpleMDE placeholder="Description" />}
+          />
 
           <div className="flex w-full space-x-4">
-          <FormField control={form.control} name="status" render={({field}) => (
-            <FormItem>
-              <FormLabel>Status</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Status" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="OPEN">Open</SelectItem>
-                  <SelectItem value="CLOSED">Closed</SelectItem>
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}></FormField>
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Status</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Status..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="OPEN">Open</SelectItem>
+                      <SelectItem value="CLOSED">Closed</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            ></FormField>
+
+            <FormField
+              control={form.control}
+              name="priority"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Priority</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Priority..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="LOW">Low</SelectItem>
+                      <SelectItem value="MEDIUM">Medium</SelectItem>
+                      <SelectItem value="HIGH">High</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            ></FormField>
           </div>
-          <Button type="submit">Submit</Button>
+          <div className="flex justify-end space-x-4">
+            <Button type="submit">Submit</Button>
+          </div>
         </form>
       </Form>
     </div>
-  )
+  );
 }
