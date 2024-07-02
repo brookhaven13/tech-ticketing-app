92b0d6c07c564f8bbdcd2733d335b9db8e25baee
Harley Lin <harley.lin@coretronic.com>
2024-07-02 14:45:17 +0800

    新增 ticket form component

---
+++ app/tickets/create/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import TicketForm from "@/components/ticket-form";
+
+export default function CreateTicket() {
+  return (
+    <div className="w-full px-10 mt-5">
+      <TicketForm />
+    </div>
+  );
+}
---
+++ components/ticket-form.tsx
@@ -0,0 +1,65 @@
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { ticketSchema } from "@/Schema/ticket";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+// Shadcn UI
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+
+type TicketFormData = z.infer<typeof ticketSchema>;
+
+async function onSubmit(values: z.infer<typeof ticketSchema>) {
+  console.log(values)
+}
+
+export default function TicketForm() {
+  const form  = useForm<TicketFormData>({
+    resolver: zodResolver(ticketSchema)
+  })
+
+  return (
+    <div className="rounded-md border w-full p-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField control={form.control} name="title" render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ticket Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Ticket title" {...field} />
+              </FormControl>
+            </FormItem>
+          )}></FormField>
+
+          <Controller control={form.control} name="description" render={({ field }) => (
+            <SimpleMDE placeholder="Description" />
+          )} />
+
+          <div className="flex w-full space-x-4">
+          <FormField control={form.control} name="status" render={({field}) => (
+            <FormItem>
+              <FormLabel>Status</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Status" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="open">Open</SelectItem>
+                  <SelectItem value="closed">Closed</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}></FormField>
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
