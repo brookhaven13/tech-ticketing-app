85c397057aff9d64049f95286ab6ed0b668097b9
Harley Lin <harley.lin@coretronic.com>
2024-07-12 13:47:03 +0800

    完成 pagination 功能

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,13 +1,67 @@
+import {
+  ChevronRight,
+  ChevronLeft,
+  ChevronFirst,
+  ChevronLast,
+} from "lucide-react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
 interface Props {
   itemCount: number;
   pageSize: number;
   currentPage: number;
 }
 
-export default function Pagination({ itemCount, pageSize, currentPage }: Props) {
+export default function Pagination({
+  itemCount,
+  pageSize,
+  currentPage,
+}: Props) {
+  const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = new URLSearchParams();
+
+  if (pageCount <= 1) {
+    return null;
+  }
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <div>
-      Pagination
+      <Button
+        variant="outline"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
+        <ChevronFirst />
+      </Button>
+      <Button
+        variant="outline"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage - 1)}
+      >
+        <ChevronLeft />
+      </Button>
+      <Button
+        variant="outline"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
+        <ChevronRight />
+      </Button>
+      <Button
+        variant="outline"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
+        <ChevronLast />
+      </Button>
     </div>
   );
 }
