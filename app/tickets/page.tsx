c41581b2c5fde69b2684dbb5d889a42233e69250
Harley Lin <harley.lin@coretronic.com>
2024-07-19 16:58:06 +0800

    完成 status filter 功能

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
-import DataTable from "./data-table";
+import prisma from "@/prisma/db";
+import { Status } from "@prisma/client";
 import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/pagination";
-import prisma from "@/prisma/db";
+import StatusFilter from "@/components/status-filter";
+import DataTable from "./data-table";
 
 interface SearchParams {
+  status?: Status;
   page: number;
 }
 
@@ -15,21 +18,38 @@ export default async function Tickets({
 }) {
   const pageSize = 10;
   const page = +searchParams?.page || 1;
-  const ticketCount = await prisma.ticket.count();
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams?.status as Status)
+    ? searchParams?.status
+    : undefined;
+
+  let where = {};
+  if (status) {
+    where = { status };
+  } else {
+    where = {
+      NOT: [{status: "CLOSED"}]
+    };
+  }
+
+  const ticketCount = await prisma.ticket.count({ where });
   const tickets = await prisma.ticket.findMany({
+    where,
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
 
   return (
     <div className="w-full flex flex-col gap-5 px-10 pb-6">
-      <div>
+      <div className="flex justify-between gap-2">
         <Link
           href={"/tickets/create"}
           className={buttonVariants({ variant: "default" })}
         >
           New Ticket
         </Link>
+        <StatusFilter />
       </div>
       <DataTable tickets={tickets} />
       <Pagination
